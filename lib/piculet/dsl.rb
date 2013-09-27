b182e6e2c2ffa5d648babff1aff8b8a782e02e5b
winebarrel <sgwr_dts@yahoo.co.jp>
2013-09-27 20:40:52 +0900

    add dsl class

--- lib/piculet/dsl.rb
+++ lib/piculet/dsl.rb
@@ -84,76 +84,91 @@ module Piculet
         end
 
         def ingress(&block)
-          @result.ingress << Rule.new(@name, :ingress, &block).result
+          @result.ingress << Permissions.new(@name, :ingress, &block).result
         end
 
         def egress(&block)
-          @result.egress << Rule.new(@name, :egress, &block).result
+          @result.egress << Permissions.new(@name, :egress, &block).result
         end
 
-        class Rule
+        class Permissions
+          attr_reader :result
+
           def initialize(security_group, direction, &block)
             @security_group = security_group
             @direction = direction
-            @result = OpenStruct.new
+            @result = {}
             instance_eval(&block)
           end
 
-          def result
-            unless @result.port_range
-              raise "SecurityGroup `#{@security_group}`: #{@direction} `port_range` is required"
-            end
-
-            unless @result.ip_ranges or @result.groups
-              raise "SecurityGroup `#{@security_group}`: #{@direction}: `ip_ranges` or `groups` is required"
-            end
-
-            @result
-          end
-
           private
-          def port_range(value)
-            unless value.kind_of?(Range)
-              raise TypeError, "SecurityGroup `#{@security_group}`: #{@direction}: can't convert #{value.class} into Range"
+          def permission(protocol, port_range = nil, &block)
+            if port_range and not port_range.kind_of?(Range)
+              raise TypeError, "SecurityGroup `#{@security_group}`: #{@direction}: can't convert #{port_range} into Range"
             end
 
-            @result.port_range = value
+            key = [protocol, port_range]
+
+            if @result.has_key?(key)
+              raise "SecurityGroup `#{@security_group}`: #{@direction}: #{key} is already defined"
+            end
+
+            @result[key] = Permission.new(@security_group, @direction, key, &block).result
           end
 
-          def ip_ranges(*values)
-            if values.empty?
-              raise ArgumentError, "SecurityGroup `#{@security_group}`: #{@direction}: `ip_ranges`: wrong number of arguments (0 for 1..)"
+          class Permission
+            def initialize(security_group, direction, protocol_prot_range, &block)
+              @security_group = security_group
+              @direction = direction
+              @protocol_prot_range = protocol_prot_range
+              @result = OpenStruct.new
+              instance_eval(&block)
             end
 
-            values.each do |ip_range|
-              unless ip_range =~ %r|\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}|
-                raise "SecurityGroup `#{@security_group}`: #{@direction}: `ip_ranges`: invalid ip range: #{ip_range}"
+            def result
+              unless @result.ip_ranges or @result.groups
+                raise "SecurityGroup `#{@security_group}`: #{@direction}: #{@protocol_prot_range}: `ip_ranges` or `groups` is required"
               end
 
-              ip, range = ip_range.split('/', 2)
-
-              unless ip.split('.').all? {|i| (0..255).include?(i.to_i) } and (0..32).include?(range.to_i)
-                raise "SecurityGroup `#{@security_group}`: #{@direction}: `ip_ranges`: invalid ip range: #{ip_range}"
-              end
+              @result
             end
 
-            @result.ip_ranges = values
-          end
+            private
+            def ip_ranges(*values)
+              if values.empty?
+                raise ArgumentError, "SecurityGroup `#{@security_group}`: #{@direction}: #{@protocol_prot_range}: `ip_ranges`: wrong number of arguments (0 for 1..)"
+              end
+
+              values.each do |ip_range|
+                unless ip_range =~ %r|\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}|
+                  raise "SecurityGroup `#{@security_group}`: #{@direction}: #{@protocol_prot_range}: `ip_ranges`: invalid ip range: #{ip_range}"
+                end
+
+                ip, range = ip_range.split('/', 2)
+
+                unless ip.split('.').all? {|i| (0..255).include?(i.to_i) } and (0..32).include?(range.to_i)
+                  raise "SecurityGroup `#{@security_group}`: #{@direction}: #{@protocol_prot_range}: `ip_ranges`: invalid ip range: #{ip_range}"
+                end
+              end
+
+              @result.ip_ranges = values
+            end
  
-          def groups(*values)
-            if values.empty?
-              raise ArgumentError, "SecurityGroup `#{@security_group}`: #{@direction}: `groups`: wrong number of arguments (0 for 1..)"
-            end
-
-            values.each do |group|
-              unless [String, Array].any? {|i| group.kind_of?(i) }
-                raise "SecurityGroup `#{@security_group}`: #{@direction}: `groups`: invalid type: #{group}"
+            def groups(*values)
+              if values.empty?
+                raise ArgumentError, "SecurityGroup `#{@security_group}`: #{@direction}: #{@protocol_prot_range}: `groups`: wrong number of arguments (0 for 1..)"
               end
-            end
 
-            @result.groups = values
-          end
-        end # Rule
+              values.each do |group|
+                unless [String, Array].any? {|i| group.kind_of?(i) }
+                  raise "SecurityGroup `#{@security_group}`: #{@direction}: #{@protocol_prot_range}: `groups`: invalid type: #{group}"
+                end
+              end
+
+              @result.groups = values
+            end
+          end # Permission
+        end # Permissions
       end # SecurityGroup
     end # EC2
   end # DSL
