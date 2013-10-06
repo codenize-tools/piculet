8a9d4443babf96fb2bc73e4f5e4aa7064c2c8ebf
winebarrel <sgwr_dts@yahoo.co.jp>
2013-10-06 19:22:54 +0900

    add spec: delete cross-reference testcase

--- spec/piculet_delete_permission_spec.rb
+++ spec/piculet_delete_permission_spec.rb
@@ -94,5 +94,64 @@ EOS
         end # it
       end # context ##################################################
     end # each #######################################################
+
+    context "delete cross-reference #{direction} permission" do #
+      before do
+        groupfile { (<<EOS)
+ec2 TEST_VPC_ID do
+  security_group "security_group_a" do
+    description "security group A"
+
+    #{direction} do
+      permission :tcp, 80..80 do
+        groups(
+          "security_group_b",
+        )
+      end # permission
+    end # ingress
+  end
+
+  security_group "security_group_b" do
+    description "security group B"
+
+    #{direction} do
+      permission :tcp, 80..80 do
+        groups(
+          "security_group_a",
+        )
+      end # permission
+    end # ingress
+  end
+
+  security_group "default" do
+    description "default VPC security group"
+  end # security_group
+end # ec2
+EOS
+        }
+      end
+
+      it do
+        groupfile { (<<EOS)
+ec2 TEST_VPC_ID do
+  security_group "default" do
+    description "default VPC security group"
+  end # security_group
+end # ec2
+EOS
+        }
+
+        exported = export_security_groups
+        expect(exported.keys).to eq([TEST_VPC_ID])
+
+        expect(exported[TEST_VPC_ID]).to eq([[
+          [:description , "default VPC security group"],
+          [:egress      , EMPTY_ARRAY],
+          [:ingress     , EMPTY_ARRAY],
+          [:name        , "default"],
+          [:owner_id    , TEST_OWNER_ID],
+        ]])
+      end # it
+    end # context ##################################################
   end # each #########################################################
 end # default
