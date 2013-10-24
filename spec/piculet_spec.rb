045ae5fd4b4dfd309a0d7e80bd1ad574272b835e
Genki Sugawara <sugawara@cookpad.com>
2013-10-24 09:50:57 +0900

    add divided config spec

--- spec/piculet_spec.rb
+++ spec/piculet_spec.rb
@@ -159,4 +159,102 @@ EOS
       ]])
     end # it
   end # context ######################################################
+
+  context 'create security group from divided config' do #############
+    it do
+      groupfile { (<<EOS)
+ec2 TEST_VPC_ID do
+  security_group "security_group_a" do
+    description "security group a"
+
+    ingress do
+      permission :any do
+        ip_ranges(
+          "0.0.0.0/0"
+        )
+      end
+    end
+
+    egress do
+      permission :any do
+        ip_ranges(
+          "0.0.0.0/0"
+        )
+      end
+    end
+  end
+
+  security_group "default" do
+    description "default VPC security group"
+  end # security_group
+end # ec2
+
+ec2 TEST_VPC_ID do
+  security_group "security_group_b" do
+    description "security group b"
+
+    ingress do
+      permission :any do
+        ip_ranges(
+          "0.0.0.0/0"
+        )
+      end
+    end
+
+    egress do
+      permission :any do
+        ip_ranges(
+          "0.0.0.0/0"
+        )
+      end
+    end
+  end
+end # ec2
+EOS
+      }
+
+      exported = export_security_groups
+      expect(exported.keys).to eq([TEST_VPC_ID])
+
+      expect(exported[TEST_VPC_ID]).to eq([[
+        [:description , "default VPC security group"],
+        [:egress      , EMPTY_ARRAY],
+        [:ingress     , EMPTY_ARRAY],
+        [:name        , "default"],
+        [:owner_id    , TEST_OWNER_ID],
+      ],[
+        [:description , "security group a"],
+        [:egress      , [[
+          [:groups     , EMPTY_ARRAY],
+          [:ip_ranges  , ["0.0.0.0/0"]],
+          [:port_range , nil],
+          [:protocol   , :any],
+        ]]],
+        [:ingress     , [[
+          [:groups     , EMPTY_ARRAY],
+          [:ip_ranges  , ["0.0.0.0/0"]],
+          [:port_range , nil],
+          [:protocol   , :any],
+        ]]],
+        [:name        , "security_group_a"],
+        [:owner_id    , TEST_OWNER_ID],
+      ],[
+        [:description , "security group b"],
+        [:egress      , [[
+          [:groups     , EMPTY_ARRAY],
+          [:ip_ranges  , ["0.0.0.0/0"]],
+          [:port_range , nil],
+          [:protocol   , :any],
+        ]]],
+        [:ingress     , [[
+          [:groups     , EMPTY_ARRAY],
+          [:ip_ranges  , ["0.0.0.0/0"]],
+          [:port_range , nil],
+          [:protocol   , :any],
+        ]]],
+        [:name        , "security_group_b"],
+        [:owner_id    , TEST_OWNER_ID],
+      ]])
+    end # it
+  end # context ######################################################
 end # default
